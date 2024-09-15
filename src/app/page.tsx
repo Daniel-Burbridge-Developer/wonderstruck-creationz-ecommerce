b476cbc2747af488c5831fd76829f64a44e9af91
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-09-15 12:31:42 +0800

    added images to homepage for testing purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HeroCarouselComponent from "@/components/hero-carousel";
 import { db } from "@/server/db";
 import { getHeroImages } from "@/server/queries";
-// import Image from "next/image";
+import Image from "next/image";
 // import Link from "next/link";
 
 export default async function HomePage() {
@@ -13,6 +13,19 @@ export default async function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <HeroCarouselComponent images={heroImages} />
+      {/* render some hero images for testing */}
+      <div className="flex flex-col">
+        {heroImages.map((image) => (
+          <div key={image.id} className="relative aspect-square w-72">
+            <Image
+              src={image.url}
+              alt={image.name}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
