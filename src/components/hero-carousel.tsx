25d621650aa7320ecb47caa5cd3ced9f1359d71c
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-09-15 12:26:30 +0800

    adjust carousel css slightly

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -31,7 +31,7 @@ export default function HeroCarouselComponent({
   }, []);
 
   return (
-    <div className="relative aspect-[21/9] max-h-[50vh] w-full overflow-hidden">
+    <div className="relative aspect-[21/9] max-h-[90vh] w-full overflow-hidden">
       {images.map((image, index) => (
         <div
           key={index}
@@ -49,6 +49,7 @@ export default function HeroCarouselComponent({
         </div>
       ))}
       <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-40 p-4 text-white">
+        <div className="h-1/2 w-1"></div>
         <h1 className="mb-4 text-center text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">
           Discover Our Refreshing Collection
         </h1>
