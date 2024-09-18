ac3f543a2c34a8f28a615a127b30bdbad942be73
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-09-18 18:28:09 +0800

    Posthog working?

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { useAuth, useUser } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import React, { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
   });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
